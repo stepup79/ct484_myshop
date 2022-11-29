49c1b43a545cd2ef27dd2b90e7d929a3ff94217c
stepup79 <duythai6995@gmail.com>
2022-11-29 11:32:07 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
+import 'ui/products/product_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,10 +24,13 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: SafeArea(
-          child: ProductDetailScreen(
-              ProductsManager().items[0],
-          ),
+      // home: SafeArea(
+      //     child: ProductDetailScreen(
+      //         ProductsManager().items[0],
+      //     ),
+      // ),
+      home: const SafeArea(
+          child: ProductsOverviewScreen(),
       ),
     );
   }
