ace3d66b62192d7a4583cb24ae92d6bae02df45d
stepup79 <duythai6995@gmail.com>
2022-11-30 03:49:27 +0700

    Xay dung trang cac dat hang

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/cart/cart_screen.dart';
+import 'ui/orders/orders_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
       //     child: ProductsOverviewScreen(),
       // ),
       home: const SafeArea(
-          child: CartScreen(),
+          child: OrdersScreen(),
       ),
     );
   }
