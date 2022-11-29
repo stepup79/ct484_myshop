4786104876410f6769ec69411ac0806a37d40f97
stepup79 <duythai6995@gmail.com>
2022-11-30 00:21:16 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
+import 'ui/cart/cart_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
       //     child: ProductsOverviewScreen(),
       // ),
       home: const SafeArea(
-          child: UserProductsScreen(),
+          child: CartScreen(),
       ),
     );
   }
